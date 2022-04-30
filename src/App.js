04f6c11738b7740e89b53dd1ed96d57a43e4ef2d
JonCabot <jfc0053@gmail.com>
2022-04-29 20:16:34 -0500

    added delete and submit state functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"; //add useState and useEffect hooks
+import React, { useState, useEffect } from "react"; //add useState and useEffect hooks
 // import './App.css';
 import Header from "./components/Header";
 import AddNew from "./components/AddNew";
@@ -7,13 +7,30 @@ import EventContainer from "./components/EventContainer";
 
 
 function App() {
+  const [events, setEvents] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/events")
+    .then((res) => res.json())
+    .then((eventsObjs) => setEvents([eventsObjs]))
+  }, []);
+
+  function onEventDelete(id) {
+    const remaingingEvents = events.filter((event) => event.id !== id)
+    setEvents([remaingingEvents])
+  };
+
+  function onEventFormSubmit(newEvent) {
+    setEvents([newEvent, ...events])
+  };
+
   return (
     <div>
       <Header />
-      <AddNew />
-      {/* <EventContainer /> pass down events from the GET and category */}
+      <AddNew onEventFormSubmit={onEventFormSubmit}/>
+      <EventContainer events={events} onEventDelete={onEventDelete}/> {/* pass down events from the GET and category */}
     </div>
   );
-}
+};
 
 export default App;
